e03c0410fef74c161d92773473e54a8f3f232e11
mrHoft <ru.hoft@gmail.com>
2024-01-10 08:28:32 +0300

    fix: nav menu layout

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -11,56 +11,58 @@ export const HeaderMenu = ({ main_menu }: { main_menu: TMainMenu[] }) => {
   const secondLevel = main_menu.filter(item => item.children)
 
   return (
-    <nav className={styles.menu__container}>
-      <ul className={styles.menu}>
-        {main_menu.map((item, index) => {
-          return (
-            <li className={styles.menu__item} key={index}>
-              <a
-                href={checkPath(item.page_slug || item.url)}
-                target={item.target_blank ? '_blank' : '_self'}
-                rel={item.target_blank ? 'noopener noreferrer' : undefined}>
-                {item.title}
-              </a>
-              {item.children && item.children.length > 0 && (
-                <ul className={styles.menu__dropdown}>
-                  {item.children.map((item, index) => {
-                    return (
-                      <li className={styles.menu__item} key={index}>
-                        <a
-                          href={checkPath(item.page_slug || item.url)}
-                          target={item.target_blank ? '_blank' : '_self'}
-                          rel={item.target_blank ? 'noopener noreferrer' : undefined}>
-                          {item.title}
-                        </a>
-                      </li>
-                    )
-                  })}
-                </ul>
-              )}
-            </li>
-          )
-        })}
-      </ul>
-      {secondLevel.length > 0 && (
+    <>
+      <nav className={styles.menu__container}>
         <ul className={styles.menu}>
-          {secondLevel.map(item => {
-            return item.children?.map((child, index) => {
-              return (
-                <li className={styles.menu__item} key={index}>
-                  <a
-                    href={checkPath(child.page_slug || child.url)}
-                    target={item.target_blank ? '_blank' : '_self'}
-                    rel={item.target_blank ? 'noopener noreferrer' : undefined}>
-                    {child.title}
-                  </a>
-                </li>
-              )
-            })
+          {main_menu.map((item, index) => {
+            return (
+              <li className={styles.menu__item} key={index}>
+                <a
+                  href={checkPath(item.page_slug || item.url)}
+                  target={item.target_blank ? '_blank' : '_self'}
+                  rel={item.target_blank ? 'noopener noreferrer' : undefined}>
+                  {item.title}
+                </a>
+                {item.children && item.children.length > 0 && (
+                  <ul className={styles.menu__dropdown}>
+                    {item.children.map((item, index) => {
+                      return (
+                        <li className={styles.menu__item} key={index}>
+                          <a
+                            href={checkPath(item.page_slug || item.url)}
+                            target={item.target_blank ? '_blank' : '_self'}
+                            rel={item.target_blank ? 'noopener noreferrer' : undefined}>
+                            {item.title}
+                          </a>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                )}
+              </li>
+            )
           })}
         </ul>
-      )}
+        {secondLevel.length > 0 && (
+          <ul className={styles.menu}>
+            {secondLevel.map(item => {
+              return item.children?.map((child, index) => {
+                return (
+                  <li className={styles.menu__item} key={index}>
+                    <a
+                      href={checkPath(child.page_slug || child.url)}
+                      target={item.target_blank ? '_blank' : '_self'}
+                      rel={item.target_blank ? 'noopener noreferrer' : undefined}>
+                      {child.title}
+                    </a>
+                  </li>
+                )
+              })
+            })}
+          </ul>
+        )}
+      </nav>
       <div className={styles.menu__dropdown_bg}></div>
-    </nav>
+    </>
   )
 }
